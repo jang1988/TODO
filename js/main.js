49b2e29e9ea39316df514bd0bda33e4e3d424dcf
Baklay Petro <pustota1988@gmail.com>
2022-07-10 11:48:57 +0300

    finalCode

--- js/main.js
+++ js/main.js
@@ -1,33 +1,23 @@
 // Находим элемент на странице
-const form = document.querySelector('#form');
+const form = document.querySelector("#form");
 
-const taskInput = document.querySelector('#taskInput')
-const tasksList = document.querySelector('#tasksList');
-const emptyList = document.querySelector('#emptyList');
+const taskInput = document.querySelector("#taskInput");
+const tasksList = document.querySelector("#tasksList");
+const emptyList = document.querySelector("#emptyList");
 
-// Добавление задачи
-form.addEventListener('submit', addTask);
-
-// Удаление задачи
-tasksList.addEventListener('click', deleteTask);
-
-// Отмечаем задачу как выполненую
-tasksList.addEventListener('click', doneTask);
-
-
-
+form.addEventListener("submit", addTask);
+tasksList.addEventListener("click", deleteTask);
+tasksList.addEventListener("click", doneTask);
 
 function addTask(event) {
-  
   // отменяем отправку формы
   event.preventDefault();
-  
+
   // Достаем текст из поля ввода
   const taskText = taskInput.value;
 
   // Формируем разметку для новой  задачи
-  const taskHTML = 
-  `<li class="list-group-item d-flex justify-content-between task-item">
+  const taskHTML = `<li class="list-group-item d-flex justify-content-between task-item">
       <span class="task-title">${taskText}</span>
       <div class="task-item__buttons">
         <button type="button" data-action="done" class="btn-action">
@@ -40,48 +30,32 @@ function addTask(event) {
   </li>`;
 
   // Добавляем задачу на страницу
-  tasksList.insertAdjacentHTML('beforeend', taskHTML);
+  tasksList.insertAdjacentHTML("beforeend", taskHTML);
 
   // Очищаям поле ввода и возвращаем на него фокус
   taskInput.value = "";
   taskInput.focus();
 
   // Если в списке более двух элементов, скрываем его
-  if(tasksList.children.length > 1) {
-    emptyList.classList.add('none');
+  if (tasksList.children.length > 1) {
+    emptyList.classList.add("none");
   }
-
-} 
-
-function deleteTask(event) {
-
-  // Проверяем что клик был по кнопке "Удалить задачу"
-  if (event.target.dataset.action === 'delete') {
-
-    const parentNode = event.target.closest('li');
-    
-    parentNode.remove();
-
-  }
-
-  if (tasksList.children.length == 1) {
-
-    emptyList.classList.remove('none');
-
-  }
-
 }
-
-function doneTask(event) {
-  
-  if (event.target.dataset.action === "done") {
-
-    const parentNode = event.target.closest('.list-group-item');
-
-    const taskTitle = parentNode.querySelector('.task-title');
-
-    taskTitle.classList.toggle('task-title--done');
-
+function deleteTask(event) {
+  // Проверяем что клик был HE по кнопке "Удалить задачу"
+  if (event.target.dataset.action !== "delete") {
+    return;
+  } else {
+    const parentNode = event.target.closest("li");
+    parentNode.remove();
+    if (tasksList.children.length == 1) {
+      emptyList.classList.remove("none");
+    }
   }
-
+}
+function doneTask(event) {
+  if (event.target.dataset.action !== "done") return
+  const parentNode = event.target.closest(".list-group-item");
+  const taskTitle = parentNode.querySelector(".task-title");
+  taskTitle.classList.toggle("task-title--done");
 }
