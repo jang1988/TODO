c5de588b54c5989a4ddd2357d9ab7c7f9a0dd467
Baklay Petro <pustota1988@gmail.com>
2022-07-10 08:42:02 +0300

    Done&ActiveBtn

--- js/main.js
+++ js/main.js
@@ -13,7 +13,8 @@ form.addEventListener('submit', addTask);
 // Удаление задачи
 tasksList.addEventListener('click', deleteTask);
 
-
+// Отмечаем задачу как выполненую
+tasksList.addEventListener('click', doneTask);
 
 
 
@@ -56,6 +57,33 @@ function addTask(event) {
 
 function deleteTask(event) {
 
+  // Проверяем что клик был по кнопке "Удалить задачу"
+  if (event.target.dataset.action === 'delete') {
+
+    const parentNode = event.target.closest('li');
+    
+    parentNode.remove();
+
+  }
+
+  if (tasksList.children.length == 1) {
+
+    emptyList.classList.remove('none');
+
+  }
+
+}
+
+function doneTask(event) {
   
+  if (event.target.dataset.action === "done") {
+
+    const parentNode = event.target.closest('.list-group-item');
+
+    const taskTitle = parentNode.querySelector('.task-title');
+
+    taskTitle.classList.toggle('task-title--done');
+
+  }
 
 }
